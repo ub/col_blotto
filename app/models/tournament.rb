a6c1e696e9880e0fd6d69f30fa70b0618187a057
Yuri Baranov <baranovu@gmail.com>
2015-05-11 19:23:12 +0300

    bug fix: reverse logic for no results

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,3 +1,11 @@
 class Tournament < ActiveRecord::Base
   has_many :entries, -> {order 'created_at, nick'}
+
+  def result
+    if open == false && entries.count > 1
+      Tournament::Result.new entries
+    else
+      Tournament::NotReady.new
+    end
+  end
 end
